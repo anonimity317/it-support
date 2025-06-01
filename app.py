5a66323ffc4f98584e899e316bd8e9226ca01aa8
anonimity317 <Joshua.S_2023_Man@outlook.com>
2025-06-01 14:46:46 +0000

    basic page layout

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-# Imports
+from website import create_app
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_scss import Scss
 
-#The App
-app = Flask(__name__)
-Scss(app)
+app = create_app()
 
-
-#Home Page of the app
 @app.route('/')
-def index():
-    return render_template("index.html")
-
+def home():
+    return render_template('index.html')
 
 if __name__ in '__main__':
     app.run(debug=True)
