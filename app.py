e97dbd081bb1f8eb504b96dd376691eb7e3261c2
anonimity317 <Joshua.S_2023_Man@outlook.com>
2025-06-03 09:03:32 +0000

    added the front end ticket table for basic users

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask, render_template
 
 app = create_app()
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
 if __name__ in '__main__':
     app.run(debug=True)
