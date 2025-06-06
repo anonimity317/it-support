8505236f7beaa88315c4720ff79c15b154bdb5b8
anonimity317 <Joshua.S_2023_Man@outlook.com>
2025-06-06 07:08:26 +0000

    html tst

--- scripts/set_admin.py
+++ scripts/set_admin.py
@@ -1,6 +1,7 @@
 from website import create_app, db
 from website.models import User
 
+# change username-'XXXXX' to elevate that user to admin
 app = create_app()
 with app.app_context():
     user = User.query.filter_by(username='admin').first()
